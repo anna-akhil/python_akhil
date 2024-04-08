a8b396e1838f46803b030c9b790eb657e8fc7ce0
anna-akhil <165661825+anna-akhil@users.noreply.github.com>
2024-04-08 15:33:19 +0530

    something i checkin

---
+++ Preprocessor/Remove_Comments.py
@@ -0,0 +1,24 @@
+# # Removing comments
+
+# # Give the File_name
+
+# file_name = "D:/python/python_akhil/Preprocessor/test.c"
+
+# File = open(file_name , "r")  # Or you can also write like   with open(file_name , "r") as File
+# count = 0
+# for k in File:
+#     count+=1
+# File.close()
+# File = open(file_name , "r")
+# # for j in range(count):
+# #     i = File.readline()
+# #     if "//" in i:
+        
+# #         print("yes")
+# #     else :
+# #         print("No")
+# re = "//"
+# update = File.(re , "")
+# with open(file_name , "w") as file:
+#     file.write(update)
+#     print(file.read())
